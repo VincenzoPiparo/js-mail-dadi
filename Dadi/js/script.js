f8520acf2e7542f023b1d178d9a276ec7ab2a681
VincenzoPiparo <v.pipar.97@gmai.com>
2021-11-17 15:55:07 +0100

    end email

---
+++ Dadi/js/script.js
@@ -0,0 +1,9 @@
+// Gioco dei dadi:
+
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+const diceUser = Math.floor(Math.random() * 6) + 1;
+console.log(diceUser)
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Che ci sia un array da qualche parte?
+// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
